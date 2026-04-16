acafe2279e0e18bf3961b7ed67ab6c4300d827a6
Marco Aurelio Hansen de Oliveira <ssnpett18@firjan.com.br>
2026-04-15 21:05:23 -0300

    adiciona o delete e o patch de usuarios

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -19,6 +19,32 @@ export class UserController {
         .json({ error: "Ocorreu um erro inesperado ao criar o usuário." });
     }
   }
+
+  async update(req: Request, res: Response) {
+    try {
+      const id = Number(req.params.id);
+      const { firstName, lastName } = req.body;
+      if (isNaN(id)) {
+        res.status(400).json({ message: "ID inválido" });
+      }
+      const user = await this.userRepository.findOneBy({ id });
+      if (!user) {
+        res.status(404).json({ message: "Usuário não encontrado!" });
+      }
+      user.firstName = firstName ?? user.firstName;
+      user.lastName = lastName ?? user.lastName;
+      await this.userRepository.save(user);
+      return res.json(user);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(400).json({ error: error.message });
+      }
+      return res
+        .status(500)
+        .json({ error: "Ocorreu um erro inesperado ao atualizar o usuário." });
+    }
+  }
+
   async list(req: Request, res: Response) {
     try {
       const users = await this.userRepository.find();
@@ -30,6 +56,26 @@ export class UserController {
       return res
         .status(500)
         .json({ error: "Ocorreu um erro inesperado ao listar os usuários." });
+    }
+  }
+  async delete(req: Request, res: Response) {
+    try {
+      const id = Number(req.params.id);
+      if (isNaN(id)) {
+        res.status(400).json({ message: "ID inválido" });
+      }
+      const result = await this.userRepository.delete(id);
+      if (result.affected === 0) {
+        res.status(404).json({ message: "Usuário não foi encontrado" });
+      }
+      return res.status(204).send();
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(400).json({ error: error.message });
+      }
+      return res
+        .status(500)
+        .json({ error: "Ocorreu um erro inesperado ao deletar o usuário." });
     }
   }
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,6 +5,8 @@ const router = Router();
 const userControler = new UserController();
 
 router.post("/", (req, res) => userControler.create(req, res));
+router.patch("/:id", (req, res) => userControler.update(req, res));
 router.get("/", (req, res) => userControler.list(req, res));
+router.delete("/:id", (req, res) => userControler.delete(req, res));
 
 export const userRoutes = router;
